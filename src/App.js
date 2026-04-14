63904e6f0aa2bc39fbee23e6bcee00060260a7d6
infoflorith-dev <infoflorith@gmail.com>
2026-04-14 08:12:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div>
+    <div style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
       <h1>Planning App</h1>
       <p>Werkt 🔥</p>
     </div>

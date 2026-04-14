be6d6a09c3af6cb7d0f51a1a83909b882eddbfb2
infoflorith-dev <infoflorith@gmail.com>
2026-04-14 19:11:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -998,7 +998,9 @@ return React.createElement(
     .map((item) => `
       <tr>
         <td>${formatHandeling(item.handeling)}</td>
-        <td>${item.extraMensen > 0 ? `${item.mensen.length} + ${item.extraMensen}` : item.mensen.length}</td>
+        <td><td>${item.extraMensen > 0
+  ? `${item.mensen.length + item.extraMensen} (${item.mensen.length} + ${item.extraMensen})`
+  : item.mensen.length}</td></td>
         <td>${item.mensen.join(", ") || "-"}</td>
         <td>${item.vervolg.length > 0 ? item.vervolg.map((v) => formatHandeling(v)).join(", ") : "-"}</td>
       </tr>

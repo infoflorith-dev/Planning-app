b9bac51b889253648f54d81e62e2f7977c7eb8f4
infoflorith-dev <infoflorith@gmail.com>
2026-04-16 06:33:18 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1342,7 +1342,7 @@ return React.createElement(
     .filter((item) => item.handeling && item.handeling.code)
     .map((item) => `
       <tr><td>
-        <td>${formatHandeling(item.handeling)}${item.klantNaam ? ` - ${item.klantNaam}` : ""}</td>
+        <td>${item.mensen.filter((naam) => !vasteMensen.includes(naam)).length + (item.extraMensen || 0)}</td>
         <<td>${item.mensen.filter((naam) => !vasteMensen.includes(naam)).length + (item.extraMensen || 0)}</td>
         <td>${[...item.mensen, ...(item.extraMensen > 0 ? [`+${item.extraMensen}`] : [])].join(", ") || "-"}</td>
         <td>${item.vervolg.length > 0 ? item.vervolg.map((v) => formatHandeling(v)).join(", ") : "-"}</td>
